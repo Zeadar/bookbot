4886751146b063c3bfc2ef4e6e711e7ff30fecb5
Zeadar <siiigsonik@gmail.com>
2024-03-16 18:29:11 +0100

    word count and report

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 def main():
-    book = get_book("./books/frankenstein.txt")
-    words = book.lower().split()
+    words = get_book("./books/frankenstein.txt").lower().split()
     #characters = [w.lower() for w in words]
     #print("letters", characters)
     print("amount of words", len(words))
     
-    word_count = {}
+    char_count = {}
 
     for w in words:
         for c in w:
-            if (c in word_count):
-                word_count[c] += 1
+            if not c.isalpha():
+                continue
+            if (c in char_count):
+                char_count[c] += 1
             else:
-                word_count[c] = 1
+                char_count[c] = 1
 
-    print("character count", word_count)
+    char_count = [{"letter": key, "num": char_count[key]} for key in char_count]
+    char_count.sort(key=get_num, reverse=True)
+    #print("character count", word_count)
+
+    for d in char_count:
+        print(d["letter"], d["num"])
+
+
+def get_num(d):
+    return d["num"]
 
 def get_book(path):
     with open(path) as file:
         return file.read()
     
-
-
 main()
